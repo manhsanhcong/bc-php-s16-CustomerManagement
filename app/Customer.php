b9b7b6a354c6748d102a9659eb4b7c802f952efe
manhsanhcong <manhsanhcong.08@gmail.com>
2018-11-19 08:57:46 +0700

    add file

---
+++ app/Customer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hoanltm
+ * Date: 16/11/2018
+ * Time: 15:15
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    //
+}
